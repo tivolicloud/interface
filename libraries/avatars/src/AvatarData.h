0005a8de740e08c2e6c1f1bfefac67b3183269ab
Stephen Birarda <commit@birarda.com>
2018-08-30 12:59:28 -0700

    cycle avatar entity IDs for new avatar mixer

--- libraries/avatars/src/AvatarData.h
+++ libraries/avatars/src/AvatarData.h
@@ -961,7 +961,8 @@ public:
     qint64 packTrait(AvatarTraits::TraitType traitType, ExtendedIODevice& destination,
                      AvatarTraits::TraitVersion traitVersion = AvatarTraits::NULL_TRAIT_VERSION);
     qint64 packTraitInstance(AvatarTraits::TraitType traitType, AvatarTraits::TraitInstanceID instanceID,
-                             ExtendedIODevice& destination, AvatarTraits::TraitVersion traitVersion = AvatarTraits::NULL_TRAIT_VERSION);
+                             ExtendedIODevice& destination, AvatarTraits::TraitVersion traitVersion = AvatarTraits::NULL_TRAIT_VERSION,
+                             AvatarTraits::TraitInstanceID wireInstanceID = AvatarTraits::TraitInstanceID());
 
     void prepareResetTraitInstances();
 
