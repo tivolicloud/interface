564c120634c34483e6337c75b70b7cafe6248d48
Caitlyn <caitlyn@flutterverse.com>
2020-03-23 10:13:05 +0000

    Updated license header data

--- libraries/entities-renderer/src/RenderableLineEntityItem.h
+++ libraries/entities-renderer/src/RenderableLineEntityItem.h
@@ -1,13 +1,20 @@
-//
-//  RenderableLineEntityItem.h
-//  libraries/entities-renderer/src/
-//
-//  Created by Seth Alves on 5/11/15.
-//  Copyright 2015 High Fidelity, Inc.
-//
-//  Distributed under the Apache License, Version 2.0.
-//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
-//
+//  Tivoli Cloud VR
+//  Copyright (C) 2020, Tivoli Cloud VR, Inc
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//  
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  
+
 
 #ifndef hifi_RenderableLineEntityItem_h
 #define hifi_RenderableLineEntityItem_h
