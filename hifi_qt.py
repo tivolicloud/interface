441e4bea0d74e11298493e292532d68e1ef9b368
Maki <mxmcube@gmail.com>
2020-03-26 18:30:33 +0000

    Updated qt link for windows with our qt build

--- hifi_qt.py
+++ hifi_qt.py
@@ -57,7 +57,8 @@ endif()
         system = platform.system()
 
         if system == 'Windows':
-            self.qtUrl = 'https://cdn.tivolicloud.com/dependencies/vcpkg/qt5-install-5.12.3-windows3.tar.gz'
+            # self.qtUrl = 'https://cdn.tivolicloud.com/dependencies/vcpkg/qt5-install-5.12.3-windows3.tar.gz'
+            self.qtUrl = 'https://cdn.tivolicloud.com/dependencies/vcpkg/tivoli-qt5-install-5.12.3-windows.tar.gz'
         
         elif system == 'Darwin':
             self.qtUrl = 'https://cdn.tivolicloud.com/dependencies/vcpkg/qt5-install-5.12.3-macos.tar.gz'
