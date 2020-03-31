304bec76d529928785d9968998ac8fa73f105f6c
Maki <mxmcube@gmail.com>
2020-03-31 20:16:41 +0100

    Updated OpenVR to v1.10.30

--- cmake/ports/openvr/portfile.cmake
+++ cmake/ports/openvr/portfile.cmake
@@ -3,43 +3,39 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ValveSoftware/openvr
-    REF v1.0.16
-    SHA512 967356563ba4232da5361510c7519d3058e09eced4571aadc00d8a75ab1f299a0aebda2b0b10b0ffb6c6a443fd718634d0c0103964e289961449c93e8d7b9d02
+    REF 26fa19eb86ab3c589af2bdbc77449d61a8ff799b # v1.10.30
+    SHA512 821e113c6a847a244cd138869b5c8192c67054e6b8d39c0764d4e88f7a839146e9d9ec1f189cd5566f8954ad07ee0c86cbf8d353806c9bceb0f0a45def1a0ca2
     HEAD_REF master
 )
 
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    set(ARCH_PATH "win64")
-elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    set(ARCH_PATH "win32")
+if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "")
+    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+        set(ARCH_PATH "win64")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+        set(ARCH_PATH "win32")
+    else()
+        message(FATAL_ERROR "Package only supports x64 and x86 Windows.")
+    endif()
+elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+        set(ARCH_PATH "linux64")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+        set(ARCH_PATH "linux32")
+    else()
+        message(FATAL_ERROR "Package only supports x64 and x86 Linux.")
+    endif()
 else()
-    message(FATAL_ERROR "Package only supports x64 and x86 windows.")
+    message(FATAL_ERROR "Package only supports Windows and Linux.")
 endif()
 
-if(VCPKG_CMAKE_SYSTEM_NAME)
-    message(FATAL_ERROR "Package only supports windows desktop.")
-endif()
+file(COPY ${SOURCE_PATH}/lib/${ARCH_PATH}/ DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+file(COPY ${SOURCE_PATH}/lib/${ARCH_PATH}/ DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
 
-file(MAKE_DIRECTORY
-    ${CURRENT_PACKAGES_DIR}/lib
-    ${CURRENT_PACKAGES_DIR}/bin
-    ${CURRENT_PACKAGES_DIR}/debug/lib
-    ${CURRENT_PACKAGES_DIR}/debug/bin
-)
-file(COPY ${SOURCE_PATH}/lib/${ARCH_PATH}/openvr_api.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(COPY ${SOURCE_PATH}/lib/${ARCH_PATH}/openvr_api.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-file(COPY
-    ${SOURCE_PATH}/bin/${ARCH_PATH}/openvr_api.dll
-    ${SOURCE_PATH}/bin/${ARCH_PATH}/openvr_api.pdb
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin
-)
-file(COPY
-    ${SOURCE_PATH}/bin/${ARCH_PATH}/openvr_api.dll
-    ${SOURCE_PATH}/bin/${ARCH_PATH}/openvr_api.pdb
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
-)
+file(COPY ${SOURCE_PATH}/bin/${ARCH_PATH}/ DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+file(COPY ${SOURCE_PATH}/bin/${ARCH_PATH}/ DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+
 file(COPY ${SOURCE_PATH}/headers DESTINATION ${CURRENT_PACKAGES_DIR})
 file(RENAME ${CURRENT_PACKAGES_DIR}/headers ${CURRENT_PACKAGES_DIR}/include)
 
