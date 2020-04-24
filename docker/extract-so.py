c6dc240b11d0debfd849aca108034fb1170bef15
Maki <mxmcube@gmail.com>
2020-04-25 00:46:21 +0100

    Server docker image with Debian 9 works now!

---
+++ docker/extract-so.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+import subprocess
+import re
+import urllib
+
+from urllib.request import urlopen
+excludelist = urlopen(
+    "https://raw.githubusercontent.com/AppImage/pkg2appimage/master/excludelist"
+).read().decode("utf-8").split("\n")
+
+def isExcluded(libName):
+	for line in excludelist:
+		if (line + " ").startswith(libName + " "):
+			return True
+	return False
+
+def cleanFolder(path):
+	if os.path.exists(path):
+		shutil.rmtree(path)
+	os.makedirs(path)
+
+def saveLibraries(executable, outputPath):
+	print("Libraries from: " + executable)
+	print("To destination: " + outputPath)
+	print("")
+
+	args = ["ldd", executable]
+	lines = subprocess.check_output(args).decode("utf-8").split("\n")
+
+	for line in lines:
+		link = re.split("=>", line)
+		if len(link) < 2:
+			continue
+
+		name = link[0].strip().split("/").pop()
+		path = re.split("\(", link[1])[0].strip()
+		if isExcluded(name):
+			print("\tEXCLUDED: " + name)
+			continue
+
+		shutil.copy2(path, outputPath)
+		print(name + " => " + path)
+	print("\n---\n")
+
+cleanFolder("_lib-server")
+saveLibraries("domain-server/domain-server", "_lib-server")
+saveLibraries("assignment-client/assignment-client", "_lib-server")
+saveLibraries("tools/oven/oven", "_lib-server")
+
+cleanFolder("_lib-ice")
+saveLibraries("ice-server/ice-server", "_lib-ice")
--- docker/server/ecosystem.config.js
+++ docker/server/ecosystem.config.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const mkdirp = dir => {
+const mkdirp = (dir) => {
     const dirArr = dir.split("/");
 
     for (const [i, folderName] of Object.entries(dirArr)) {
@@ -46,8 +46,8 @@ try {
         domainServerConfigPath,
         JSON.stringify({
             metaverse: {
-                local_port: domainServerPort
-            }
+                local_port: domainServerPort,
+            },
         })
     );
 }
@@ -60,46 +60,58 @@ const assignmentClientPath = "/root/assignment-client";
 const port = (name, fallback) =>
     process.env["HIFI_ASSIGNMENT_CLIENT_" + name + "_PORT"] || fallback;
 
+const env = {
+    LD_LIBRARY_PATH: "/root/lib",
+};
+
 module.exports = {
     apps: [
         {
             name: "Domain Server",
-            script: domainServerPath
+            script: domainServerPath,
+            env,
         },
         {
             name: "Audio Mixer",
             script: assignmentClientPath,
-            args: "-t 0 -a localhost -p " + port("AUDIO_MIXER", 48000)
+            args: "-t 0 -a localhost -p " + port("AUDIO_MIXER", 48000),
+            env,
         },
         {
             name: "Avatar Mixer",
             script: assignmentClientPath,
-            args: "-t 1 -a localhost -p " + port("AVATAR_MIXER", 48001)
+            args: "-t 1 -a localhost -p " + port("AVATAR_MIXER", 48001),
+            env,
         },
         {
             name: "Scripted Agent",
             script: assignmentClientPath,
-            args: "-t 2 -a localhost --max 100"
+            args: "-t 2 -a localhost --max 100",
+            env,
         },
         {
             name: "Asset Server",
             script: assignmentClientPath,
-            args: "-t 3 -a localhost -p " + port("ASSET_SERVER", 48003)
+            args: "-t 3 -a localhost -p " + port("ASSET_SERVER", 48003),
+            env,
         },
         {
             name: "Messages Mixer",
             script: assignmentClientPath,
-            args: "-t 4 -a localhost -p " + port("MESSAGES_MIXER", 48004)
+            args: "-t 4 -a localhost -p " + port("MESSAGES_MIXER", 48004),
+            env,
         },
         {
             name: "Entity Script Server",
             script: assignmentClientPath,
-            args: "-t 5 -a localhost -p " + port("ENTITY_SCRIPT_SERVER", 48005)
+            args: "-t 5 -a localhost -p " + port("ENTITY_SCRIPT_SERVER", 48005),
+            env,
         },
         {
             name: "Entities Server",
             script: assignmentClientPath,
-            args: "-t 6 -a localhost -p " + port("ENTITIES_SERVER", 48006)
-        }
-    ]
+            args: "-t 6 -a localhost -p " + port("ENTITIES_SERVER", 48006),
+            env,
+        },
+    ],
 };
