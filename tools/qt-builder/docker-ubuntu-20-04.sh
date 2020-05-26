8270eb09b40ed1f16e7b87f5aa60c99fb202fbc3
Maki <mxmcube@gmail.com>
2020-05-26 23:00:07 +0100

    Added docker-ubuntu-20-04.sh

---
+++ tools/qt-builder/docker-ubuntu-20-04.sh
@@ -0,0 +1,6 @@
+docker run -it --rm \
+-v $PWD/patches:/patches:ro \
+-v $PWD/ubuntu-20-04/qt5:/qt5 \
+-v $PWD/ubuntu-20-04/qt5-build:/qt5-build \
+-v $PWD/ubuntu-20-04/qt5-install:/qt5-install \
+ubuntu:20.04
