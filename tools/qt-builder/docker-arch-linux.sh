f636ed7cdb87d9da14557280f13a405d8652ae77
Maki <mxmcube@gmail.com>
2020-05-26 23:09:55 +0100

    Point all qt-builder docker scripts to same qt5 folder

--- tools/qt-builder/docker-arch-linux.sh
+++ tools/qt-builder/docker-arch-linux.sh
@@ -1,6 +1,6 @@
 docker run -it --rm \
 -v $PWD/patches:/patches:ro \
--v $PWD/arch-linux/qt5:/qt5 \
+-v $PWD/qt5:/qt5 \
 -v $PWD/arch-linux/qt5-build:/qt5-build \
 -v $PWD/arch-linux/qt5-install:/qt5-install \
 archlinux:latest
--- tools/qt-builder/docker-debian-9.sh
+++ tools/qt-builder/docker-debian-9.sh
@@ -2,7 +2,7 @@
 
 docker run -it --rm \
 -v $PWD/patches:/patches:ro \
--v $PWD/debian-9/qt5:/qt5 \
+-v $PWD/qt5:/qt5 \
 -v $PWD/debian-9/qt5-build:/qt5-build \
 -v $PWD/debian-9/qt5-install:/qt5-install \
 debian:9
