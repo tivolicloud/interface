dd97256d5f1b84a0b3268b0aa6c56fed403d1acd
Caitlyn <caitlyn@flutterverse.com>
2020-06-26 14:15:07 +0100

    Removed 'P' snapshot shortcut (interferes with text chat)

--- scripts/system/keyboardShortcuts/keyboardShortcuts.js
+++ scripts/system/keyboardShortcuts/keyboardShortcuts.js
@@ -11,21 +11,14 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
-(function () { // BEGIN LOCAL_SCOPE
+(function () { // BEGIN LOCAL_SCOPE 
     var snapActivateSound = SoundCache.getSound(Script.resourcesPath() + "sounds/snapshot/snap.wav");
     function keyPressEvent(event) {
         if (event.text.toUpperCase() === "B" && event.isControl && !event.isShifted && !event.isAlt && !event.isCommand) {
             Window.openWebBrowser();
         } else if (event.text.toUpperCase() === "N" && event.isControl && !event.isShifted && !event.isAlt && !event.isCommand) {
             Users.toggleIgnoreRadius();
-        } else if (event.text.toUpperCase() === "P" && !event.isControl && !event.isShifted && !event.isAlt && !event.isCommand) {
-            Audio.playSound(snapActivateSound, {
-                position: { x: MyAvatar.position.x, y: MyAvatar.position.y, z: MyAvatar.position.z },
-                localOnly: true,
-                volume: 0.5
-            });
-            Window.takeSnapshot(true);
-        }
+        } 
     }
 
     function scriptEnding() {
