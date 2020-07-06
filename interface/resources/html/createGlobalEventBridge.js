9492f503b3addd2cf1f24eed33dd7a1c925e47a9
Caitlyn <caitlyn@flutterverse.com>
2020-07-06 03:07:17 +0100

    Setting audio device correctly for web entities

--- interface/resources/html/createGlobalEventBridge.js
+++ interface/resources/html/createGlobalEventBridge.js
@@ -27,48 +27,67 @@ var EventBridge;
         };
     };
 
-    function setAudioDevice(deviceName) {
-        navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(function(mediaStream) {
-            navigator.mediaDevices.enumerateDevices().then(function(devices) {
-                devices.forEach(function(device) {
-                    if (device.kind == "audiooutput") {
-                        if (device.label == deviceName){
-                            console.log("Changing HTML audio output to device " + device.label);
-                            var deviceId = device.deviceId;
-                            var videos = document.getElementsByTagName("video");
-                            for (var i = 0; i < videos.length; i++){
-                                videos[i].setSinkId(deviceId);
-                            }
-                            var audios = document.getElementsByTagName("audio");
-                            for (var i = 0; i < audios.length; i++){
-                                audios[i].setSinkId(deviceId);
-                            }
-                        }
-                    }
-                });
-
-            }).catch(function(err) {
-                console.log("Error getting media devices"+ err.name + ": " + err.message);
-            });
-        }).catch(function(err) {
-            console.log("Error getting user media"+ err.name + ": " + err.message);
-        });
-    }
-
     EventBridge = new TempEventBridge();
 
-    var webChannel = new QWebChannel(qt.webChannelTransport, function (channel) {
+    const webChannel = new QWebChannel(qt.webChannelTransport, function (channel) {
         // replace the TempEventBridge with the real one.
-        var tempEventBridge = EventBridge;
+        const tempEventBridge = EventBridge;
         EventBridge = channel.objects.eventBridge;
         
-        EventBridge.audioOutputDeviceChanged.connect(function(deviceName) {      
-            setAudioDevice(deviceName);
-        });
-        EventBridge.getCurrentAudioOutputDevice().then(function(deviceName) {
-            console.log("_currentAudioOutputDevice", deviceName);
-            setAudioDevice(deviceName);
-        });
+        EventBridge.audioOutputDeviceChanged.connect(async (deviceName) => {
+			deviceName = deviceName.trim().toLowerCase();
+			if (deviceName == "") deviceName = "default";
+
+			await navigator.mediaDevices
+				.getUserMedia({ audio: true, video: false })
+				.catch((err) => {
+					console.error(
+						"Error getting media devices" +
+							err.name +
+							": " +
+							err.message
+					);
+				});
+                
+			const devices = await navigator.mediaDevices
+				.enumerateDevices()
+				.catch((err) => {
+					console.error(
+						"Error getting user media" +
+							err.name +
+							": " +
+							err.message
+					);
+				});
+
+			const device = devices
+				.filter((device) => device.kind == "audiooutput")
+				.find((device) =>
+					deviceName == "default"
+						? device.deviceId == "default"
+						: deviceName ==
+						  device.label
+								.replace(/\([0-9a-f]{4}:[0-9a-f]{4}\)/gi, "")
+								.trim()
+								.toLowerCase()
+				);
+
+			if (device == null) {
+				console.error("Failed to change HTML audio output device");
+				return;
+			} else {
+				console.log(
+					"Changing HTML audio output to device " + device.label
+				);
+			}
+
+			for (const video of document.getElementsByTagName("video")) {
+				video.setSinkId(device.deviceId);
+			}
+			for (const audio of document.getElementsByTagName("audio")) {
+				audio.setSinkId(device.deviceId);
+			}
+		});
         
         // To be able to update the state of the output device selection for every element added to the DOM
         // we need to listen to events that might precede the addition of this elements.
