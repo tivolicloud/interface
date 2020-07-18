6c7602b33a61d21bbf4d23f7df81e6524de8cee6
Maki <mxmcube@gmail.com>
2020-07-18 02:06:41 +0100

    Improved chat emoji loading system

--- tivoli-scripts/frontend/src/app/chat/emoji-menu/emoji-menu.component.ts
+++ tivoli-scripts/frontend/src/app/chat/emoji-menu/emoji-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
 import { EmojiService } from "../../emoji.service";
 
 @Component({
@@ -7,9 +7,19 @@ import { EmojiService } from "../../emoji.service";
 	styleUrls: ["./emoji-menu.component.scss"],
 })
 export class EmojiMenuComponent implements OnInit {
+	@Input() open = false;
 	@Output() emojiShortcode = new EventEmitter<string>();
 
 	// itemSize = (32 + 2 + 2) / 9;
+
+	private _loaded = false;
+	get loaded() {
+		if (this._loaded) return true;
+		if (this.open) {
+			this._loaded = true;
+			return true;
+		}
+	}
 
 	constructor(public readonly emojiService: EmojiService) {}
 
