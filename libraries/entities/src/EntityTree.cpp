acce30b0c0b4085c1f434d3684cbe72b90aa1789
Maki <mxmcube@gmail.com>
2020-10-04 19:34:33 +0100

    Revert "Revert "abide by domain-entity hierarchy deletion rules""
    This reverts commit 03c20909ee940ac7b657818ca83e87cf9d78a8e3.

--- libraries/entities/src/EntityTree.cpp
+++ libraries/entities/src/EntityTree.cpp
@@ -740,7 +740,6 @@ void EntityTree::deleteEntitiesByID(const QSet<EntityItemID>& ids, bool force, b
         });
     } else {
         SetOfEntities entitiesToDelete;
-        SetOfEntities domainEntities;
         QUuid sessionID = DependencyManager::get<NodeList>()->getSessionUUID();
         withWriteLock([&] {
             for (auto id : ids) {
@@ -751,10 +750,12 @@ void EntityTree::deleteEntitiesByID(const QSet<EntityItemID>& ids, bool force, b
                 }
                 if (entity) {
                     if (entity->isDomainEntity()) {
-                        domainEntities.insert(entity);
+                        if (_simulation) {
+                            _simulation->queueEraseDomainEntity(entity->getID());
+                        }
                     } else if (entity->isLocalEntity() || entity->isMyAvatarEntity()) {
                         entitiesToDelete.insert(entity);
-                        entity->collectChildrenForDelete(entitiesToDelete, domainEntities, sessionID);
+                        entity->collectChildrenForDelete(entitiesToDelete, sessionID);
                     }
                 }
             }
@@ -762,10 +763,6 @@ void EntityTree::deleteEntitiesByID(const QSet<EntityItemID>& ids, bool force, b
                 deleteEntitiesByPointer(entitiesToDelete);
             }
         });
-        if (!domainEntities.empty() && _simulation) {
-            // interface-client can't delete domainEntities outright, they must roundtrip through the entity-server
-            _simulation->queueEraseDomainEntities(domainEntities);
-        }
     }
 }
 
@@ -2464,7 +2461,6 @@ int EntityTree::processEraseMessage(ReceivedMessage& message, const SharedNodePo
     #ifdef EXTRA_ERASE_DEBUGGING
         qCDebug(entities) << "EntityTree::processEraseMessage()";
     #endif
-    SetOfEntities consequentialDomainEntities;
     withWriteLock([&] {
         message.seek(sizeof(OCTREE_PACKET_FLAGS) + sizeof(OCTREE_PACKET_SEQUENCE) + sizeof(OCTREE_PACKET_SENT_TIME));
 
@@ -2511,7 +2507,7 @@ int EntityTree::processEraseMessage(ReceivedMessage& message, const SharedNodePo
             SetOfEntities entitiesToDelete;
             for (auto entity : domainEntities) {
                 entitiesToDelete.insert(entity);
-                entity->collectChildrenForDelete(entitiesToDelete, consequentialDomainEntities, sessionID);
+                entity->collectChildrenForDelete(entitiesToDelete, sessionID);
             }
 
             if (!entitiesToDelete.empty()) {
@@ -2519,9 +2515,6 @@ int EntityTree::processEraseMessage(ReceivedMessage& message, const SharedNodePo
             }
         }
     });
-    if (!consequentialDomainEntities.empty() && _simulation) {
-        _simulation->queueEraseDomainEntities(consequentialDomainEntities);
-    }
     return message.getPosition();
 }
 
