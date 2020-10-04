5d679e0c875d2347d439ad60cc83e272244140f9
Maki <mxmcube@gmail.com>
2020-10-04 21:43:39 +0100

    Procedural Shader applies to entities other than target

--- libraries/entities-renderer/src/RenderableShapeEntityItem.cpp
+++ libraries/entities-renderer/src/RenderableShapeEntityItem.cpp
@@ -32,7 +32,7 @@ bool ShapeEntityRenderer::needsRenderUpdate() const {
     if (resultWithReadLock<bool>([&] {
             auto mat = _materials.find("0");
             if (mat != _materials.end() && mat->second.top().material && mat->second.top().material->isProcedural() &&
-                mat->second.top().material->isEnabled()) {
+                mat->second.top().material->isReady()) {
                 auto procedural = std::static_pointer_cast<graphics::ProceduralMaterial>(mat->second.top().material);
                 if (procedural->isFading()) {
                     return true;
@@ -94,7 +94,7 @@ void ShapeEntityRenderer::doRenderUpdateAsynchronousTyped(const TypedEntityPoint
     withReadLock([&] {
         auto mat = _materials.find("0");
         if (mat != _materials.end() && mat->second.top().material && mat->second.top().material->isProcedural() &&
-            mat->second.top().material->isEnabled()) {
+            mat->second.top().material->isReady()) {
             auto procedural = std::static_pointer_cast<graphics::ProceduralMaterial>(mat->second.top().material);
             if (procedural->isFading()) {
                 procedural->setIsFading(Interpolate::calculateFadeRatio(procedural->getFadeStartTime()) < 1.0f);
@@ -146,7 +146,7 @@ bool ShapeEntityRenderer::isTransparent() const {
 
     auto mat = _materials.find("0");
     if (mat != _materials.end() && mat->second.top().material) {
-        if (mat->second.top().material->isProcedural() && mat->second.top().material->isEnabled()) {
+        if (mat->second.top().material->isProcedural() && mat->second.top().material->isReady()) {
             auto procedural = std::static_pointer_cast<graphics::ProceduralMaterial>(mat->second.top().material);
             if (procedural->isFading()) {
                 return true;
