c0439cc14b4eb892f84aad9e3a1da4014ecf6034
Maki <mxmcube@gmail.com>
2020-10-16 18:49:49 +0100

    Removed unnecessary bootstrap environment variable for vcpkg on macOS

--- hifi_vcpkg.py
+++ hifi_vcpkg.py
@@ -97,7 +97,6 @@ endif()
         elif 'Darwin' == system:
             self.exe = os.path.join(self.path, 'vcpkg')
             self.bootstrapCmds = [ os.path.join(self.path, 'bootstrap-vcpkg.sh'), '-disableMetrics' ]
-            self.bootstrapEnv["MACOSX_DEPLOYMENT_TARGET"] = "10.15" # just for bootstrapping
             # self.vcpkgUrl = 'https://cdn.tivolicloud.com/dependencies/vcpkg/vcpkg-osx-client.tar'
             # self.vcpkgHash = '519d666d02ef22b87c793f016ca412e70f92e1d55953c8f9bd4ee40f6d9f78c1df01a6ee293907718f3bbf24075cc35492fb216326dfc50712a95858e9cbcb4d'
             self.hostTriplet = 'x64-osx'
