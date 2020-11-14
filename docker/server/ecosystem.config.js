d6136818827ff1184779dc439fe051e424953da7
Maki <mxmcube@gmail.com>
2020-11-14 00:48:31 +0000

    Added auto updater to world server

--- docker/server/ecosystem.config.js
+++ docker/server/ecosystem.config.js
@@ -54,62 +54,56 @@ try {
 
 // start services
 
+const autoUpdaterPath = "/root/auto-updater.js";
 const domainServerPath = "/root/server.AppDir/domain-server";
 const assignmentClientPath = "/root/server.AppDir/assignment-client";
 
 const port = (name, fallback) =>
 	process.env["HIFI_ASSIGNMENT_CLIENT_" + name + "_PORT"] || fallback;
 
-const env = {};
-
 module.exports = {
 	apps: [
+		...(process.env["AUTO_UPDATE_TIME"] != null
+			? [{ name: "Auto Updater", script: autoUpdaterPath }]
+			: []),
 		{
 			name: "Domain Server",
 			script: domainServerPath,
-			env,
 		},
 		{
 			name: "Audio Mixer",
 			script: assignmentClientPath,
 			args: "-t 0 -a localhost -p " + port("AUDIO_MIXER", 48000),
-			env,
 		},
 		{
 			name: "Avatar Mixer",
 			script: assignmentClientPath,
 			args: "-t 1 -a localhost -p " + port("AVATAR_MIXER", 48001),
-			env,
 		},
 		{
 			name: "Scripted Agent",
 			script: assignmentClientPath,
 			args: "-t 2 -a localhost --max 100",
-			env,
 		},
 		{
 			name: "Asset Server",
 			script: assignmentClientPath,
 			args: "-t 3 -a localhost -p " + port("ASSET_SERVER", 48003),
-			env,
 		},
 		{
 			name: "Messages Mixer",
 			script: assignmentClientPath,
 			args: "-t 4 -a localhost -p " + port("MESSAGES_MIXER", 48004),
-			env,
 		},
 		{
 			name: "Entity Script Server",
 			script: assignmentClientPath,
 			args: "-t 5 -a localhost -p " + port("ENTITY_SCRIPT_SERVER", 48005),
-			env,
 		},
 		{
 			name: "Entities Server",
 			script: assignmentClientPath,
 			args: "-t 6 -a localhost -p " + port("ENTITIES_SERVER", 48006),
-			env,
 		},
 	],
 };
