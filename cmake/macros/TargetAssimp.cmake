c3cadbb730ba7dd5e1751e1f8e726f9316384590
Maki <mxmcube@gmail.com>
2021-01-07 01:13:18 +0000

    Fixed compiling on Windows and macOS

--- cmake/macros/TargetAssimp.cmake
+++ cmake/macros/TargetAssimp.cmake
@@ -1,10 +1,19 @@
 macro(TARGET_ASSIMP)
-    find_library(ASSIMP_LIBRARY_RELEASE assimp PATHS ${VCPKG_INSTALL_ROOT}/lib NO_DEFAULT_PATH)
-    find_library(ASSIMP_LIBRARY_DEBUG assimp PATHS ${VCPKG_INSTALL_ROOT}/debug/lib NO_DEFAULT_PATH)
+    set(ASSIMP_EXTRA_NAMES)
+    if (WIN32)
+        # was assimp-vc142-mt for me, but could be different for others
+        foreach(VC_VERSION RANGE 100 399)
+            list(APPEND ASSIMP_EXTRA_NAMES assimp-vc${VC_VERSION}-mt)
+        endforeach()
+    endif()
+
+    find_library(ASSIMP_LIBRARY_RELEASE assimp ${ASSIMP_EXTRA_NAMES} PATHS ${VCPKG_INSTALL_ROOT}/lib NO_DEFAULT_PATH)
+    find_library(ASSIMP_LIBRARY_DEBUG assimp ${ASSIMP_EXTRA_NAMES} PATHS ${VCPKG_INSTALL_ROOT}/debug/lib NO_DEFAULT_PATH)
     select_library_configurations(ASSIMP)
 
-    target_link_libraries(${TARGET_NAME}
-        ${ASSIMP_LIBRARIES}
-        Xxf86vm
-    )
+    target_link_libraries(${TARGET_NAME} ${ASSIMP_LIBRARIES})
+
+    if (UNIX AND NOT APPLE AND NOT ANDROID)
+        target_link_libraries(${TARGET_NAME} Xxf86vm)
+    endif()
 endmacro()
--- cmake/macros/TargetBrotli.cmake
+++ cmake/macros/TargetBrotli.cmake
@@ -1,14 +1,14 @@
 macro(TARGET_BROTLI)
-    find_library(BROTLI_COMMON_LIBRARY_RELEASE brotlicommon-static PATHS ${VCPKG_INSTALL_ROOT}/lib NO_DEFAULT_PATH)
-    find_library(BROTLI_COMMON_LIBRARY_DEBUG brotlicommon-static PATHS ${VCPKG_INSTALL_ROOT}/debug/lib NO_DEFAULT_PATH)
+    find_library(BROTLI_COMMON_LIBRARY_RELEASE brotlicommon-static brotlicommon PATHS ${VCPKG_INSTALL_ROOT}/lib NO_DEFAULT_PATH)
+    find_library(BROTLI_COMMON_LIBRARY_DEBUG brotlicommon-static brotlicommon PATHS ${VCPKG_INSTALL_ROOT}/debug/lib NO_DEFAULT_PATH)
 	select_library_configurations(BROTLI_COMMON)
 
-    find_library(BROTLI_DECODE_LIBRARY_RELEASE brotlidec-static PATHS ${VCPKG_INSTALL_ROOT}/lib NO_DEFAULT_PATH)
-    find_library(BROTLI_DECODE_LIBRARY_DEBUG brotlidec-static PATHS ${VCPKG_INSTALL_ROOT}/debug/lib NO_DEFAULT_PATH)
+    find_library(BROTLI_DECODE_LIBRARY_RELEASE brotlidec-static brotlidec PATHS ${VCPKG_INSTALL_ROOT}/lib NO_DEFAULT_PATH)
+    find_library(BROTLI_DECODE_LIBRARY_DEBUG brotlidec-static brotlidec PATHS ${VCPKG_INSTALL_ROOT}/debug/lib NO_DEFAULT_PATH)
 	select_library_configurations(BROTLI_DECODE)
 	
-    find_library(BROTLI_ENCODE_LIBRARY_RELEASE brotlienc-static PATHS ${VCPKG_INSTALL_ROOT}/lib NO_DEFAULT_PATH)
-    find_library(BROTLI_ENCODE_LIBRARY_DEBUG brotlienc-static PATHS ${VCPKG_INSTALL_ROOT}/debug/lib NO_DEFAULT_PATH)
+    find_library(BROTLI_ENCODE_LIBRARY_RELEASE brotlienc-static brotlienc PATHS ${VCPKG_INSTALL_ROOT}/lib NO_DEFAULT_PATH)
+    find_library(BROTLI_ENCODE_LIBRARY_DEBUG brotlienc-static brotlienc PATHS ${VCPKG_INSTALL_ROOT}/debug/lib NO_DEFAULT_PATH)
     select_library_configurations(BROTLI_ENCODE)
 
 	target_link_libraries(${TARGET_NAME}
