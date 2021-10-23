966be1c812b776ac8ff37b4af5a6703b29e5d9a8
Maki <mxmcube@gmail.com>
2021-10-22 19:45:40 -0500

    Fixed compiling draco 1.3.5 with gcc 11

--- cmake/ports/draco/portfile.cmake
+++ cmake/ports/draco/portfile.cmake
@@ -6,7 +6,9 @@ vcpkg_from_github(
     REF 1.3.5
     SHA512 f99fcbec60fbd1683d8aacc35ff8ad9ee1c84374132ad4cc8c0f56662f5d33f940f89028cf3e577cde3314fd0766c124f61798121e4127e888f302e9efe1a004
     HEAD_REF master
-    PATCHES signed-unsigned-mismatch.patch
+    PATCHES
+        signed-unsigned-mismatch.patch
+        fix-gcc11.patch
 )
 
 vcpkg_configure_cmake(
